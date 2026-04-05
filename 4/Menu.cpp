29b5fee5de19865a6f86e3d50d115d88753f02e9
azozulyuk <kvitochka101010@gmail.com>
2026-04-05 01:56:34 -0400

    Remove commented citation and source information

--- 4/Menu.cpp
+++ 4/Menu.cpp
@@ -40,23 +40,22 @@ namespace seneca {
         }
         return *this;
     }
-  int Menu::run() const {
-    int choice;
-    cout << m_title << endl;
+    int Menu::run() const {
+        int choice;
+        cout << m_title << " " << endl;
 
+        for (int i = 0; i < m_count; i++) {
+            for (int j = 0; j < m_indent; j++) cout << ' ';
+            cout << (i + 1) << "- " << m_items[i] << endl;
+        }
 
-    for (int i = 0; i < m_count; i++) {
-        for (int j = 0; j < m_indent; j++) cout << ' ';
-        cout << (i + 1) << "- " << m_items[i] << endl;
+        for (int i = 0; i < m_indent; i++) cout << ' ';
+        cout << "0- " << m_exit << endl;
+
+        for (int i = 0; i < m_indent; i++) cout << ' ';
+        cout << "> ";
+
+        cin >> choice;
+        return choice;
     }
-
-    for (int i = 0; i < m_indent; i++) cout << ' ';
-    cout << "0- " << m_exit << endl;
-
-    for (int i = 0; i < m_indent; i++) cout << ' ';
-    cout << "> ";
-
-    cin >> choice;
-    return choice;
-}
 }
